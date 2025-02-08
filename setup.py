f53acfd3dbae029120d233be3e886447ae3c76fe
M Nithin <mnithinsavi@gmail.com>
2025-02-08 19:17:23 +0530

    New ui change

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-
-      name = 'Generative-AI-Project',
-
-      version= '0.0.0',
-
-      author= 'Roshni S',
-
-      author_email= 'roshnickbaby@gmail.com',
-
-      packages= find_packages(),
-
-      install_requires = []
-
+    name="Generative-AI-Project",
+    version="0.0.0",
+    author="Roshni S",
+    author_email="mnithinsavi@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
 )
+    
